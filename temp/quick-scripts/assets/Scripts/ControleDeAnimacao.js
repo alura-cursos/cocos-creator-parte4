e55f80c0db26464622cc8dca36dcb7a20ca77621
Ricardo Bugan <dones.bugan@gmail.com>
2018-01-16 15:17:40 -0200

    Projeto aula 5

--- temp/quick-scripts/assets/Scripts/ControleDeAnimacao.js
+++ temp/quick-scripts/assets/Scripts/ControleDeAnimacao.js
@@ -8,27 +8,46 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        _animacao: cc.Animation
+        anguloDiagonal: cc.Float,
+        _animacao: cc.Animation,
+        _senoMinimo: cc.Float,
+        _cossenoMinimo: cc.Float
+
     },
 
     onLoad: function onLoad() {
         this._animacao = this.getComponent(cc.Animation);
         this._animacao.play("AndarBaixo");
+
+        this._senoMinimo = Math.sin(cc.degreesToRadians(this.anguloDiagonal));
+        this._cossenoMinimo = Math.cos(cc.degreesToRadians(90 - this.anguloDiagonal));
     },
 
     mudaAnimacao: function mudaAnimacao(direcao, estado) {
         var proximaAnimacao = estado;
 
-        if (direcao.x > 0) {
-            proximaAnimacao += "Direita";
-        } else if (direcao.x < 0) {
-            proximaAnimacao += "Esquerda";
+        var angulo = Math.atan2(direcao.y, direcao.x);
+
+        var cosseno = Math.cos(angulo);
+        cosseno = Math.abs(cosseno);
+
+        var seno = Math.sin(angulo);
+        seno = Math.abs(seno);
+
+        if (cosseno > this._cossenoMinimo) {
+            if (direcao.x > 0) {
+                proximaAnimacao += "Direita";
+            } else if (direcao.x < 0) {
+                proximaAnimacao += "Esquerda";
+            }
         }
 
-        if (direcao.y > 0) {
-            proximaAnimacao += "Cima";
-        } else if (direcao.y < 0) {
-            proximaAnimacao += "Baixo";
+        if (seno > this._senoMinimo) {
+            if (direcao.y > 0) {
+                proximaAnimacao += "Cima";
+            } else if (direcao.y < 0) {
+                proximaAnimacao += "Baixo";
+            }
         }
 
         if (!this._animacao.getAnimationState(proximaAnimacao).isPlaying) {
